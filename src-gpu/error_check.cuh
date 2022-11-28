99b320781d12cba76e6bc297c64e10f33f5f8618
Eric Yuan <116010276@link.cuhk.edu.cn>
2022-11-27 21:22:31 -0800

    base cuda data struct

--- src-gpu/error_check.cuh
+++ src-gpu/error_check.cuh
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdio.h>
+
 #define CUDAERR(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
 {
--- src-gpu/main.cu
+++ src-gpu/main.cu
@@ -2,8 +2,8 @@
 
 
 int main() {
+  u_int test = 3;
+  CSRMatDevice<float> a_mat(test, test, test);
   
-
-
   return 0;
 }
