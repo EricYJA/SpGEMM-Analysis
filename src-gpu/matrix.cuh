d34997ac51506d2501b4ffbc19cfd3e9eb0e1e97
Eric Yuan <116010276@link.cuhk.edu.cn>
2022-11-27 21:34:04 -0800

    rename

--- src-gpu/matrix.cuh
+++ src-gpu/matrix.cuh
@@ -25,3 +25,27 @@ struct CSRMatDevice
   u_int m_col_size;
   u_int m_nnz;
 };
+
+
+
+template <typename T>
+struct CSCMatDevice
+{
+  CSCMatDevice(u_int row_size, u_int col_size, u_int nnz_size)
+      : m_row_size(row_size),
+        m_col_size(col_size),
+        m_nnz(nnz_size)
+  {
+    CUDAERR(cudaMalloc(&m_d_colptr, m_col_size * sizeof(T)));
+    CUDAERR(cudaMalloc(&m_d_rowidx, m_nnz * sizeof(T)));
+    CUDAERR(cudaMalloc(&m_d_val, m_nnz * sizeof(T)));
+  }
+
+  u_int *m_d_colptr;
+  u_int *m_d_rowidx;
+  T *m_d_val;
+
+  u_int m_row_size;
+  u_int m_col_size;
+  u_int m_nnz;
+};
