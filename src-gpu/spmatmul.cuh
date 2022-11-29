212f7329c0436743e4b652ae053242ea326c3bca
Eric Yuan <116010276@link.cuhk.edu.cn>
2022-11-29 11:27:27 -0800

    count nnz

--- src-gpu/spmatmul.cuh
+++ src-gpu/spmatmul.cuh
@@ -3,17 +3,42 @@
 #include "matrix.cuh"
 
 template <typename T>
-__global__ void spgemmRowWiseNnzKernel(CSRMatDevice<T> a_mat, CSRMatDevice<T> b_mat, COOMatDevice<T> c_mat) {
-  
+__global__ void countNnzKernel(CSRMatDevice<T> a_mat, CSRMatDevice<T> b_mat, int *nnz_num)
+{
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (idx < a_mat.m_row_size)
+  {
+    u_int mask[b_mat.m_row_size];
+
+    u_int a_ci_s = a_mat.m_d_rowptr[idx];
+    u_int a_ci_e = a_mat.m_d_rowptr[idx + 1];
+
+    for (int i = a_ci_s; i < a_ci_e; ++i)
+    {
+      u_int b_ci_s = b_mat.m_d_rowptr[a_mat.m_d_colidx[i]];
+      u_int b_ci_e = b_mat.m_d_rowptr[a_mat.m_d_colidx[i] + 1];
+      for (int j = b_ci_s; j < b_ci_e; ++j)
+      {
+        int nz_idx = b_mat.m_d_colidx[j];
+        if (mask[nz_idx] != 1)
+        {
+          mask[k] = 1;
+          atomicAdd(&nnz_num[0], 1);
+        }
+      }
+    }
+  }
 }
 
 template <typename T>
-void spgemmRowWiseMul(CSRMatDevice<T> a_mat, CSRMatDevice<T> b_mat, COOMatDevice<T> c_mat) {
-  u_int c_nnz = a_mat.m_nnz > b_mat.m_nnz ? a_mat.m_nnz : b_mat.m_nnz;
-  
-  
+void spgemmRowWiseMul(CSRMatDevice<T> a_mat, CSRMatDevice<T> b_mat, COOMatDevice<T> c_mat)
+{
+  int *c_nnz;
+  cudaMallocManaged(&c_nnz, sizeof(int));
+  countNnzKernel(a_mat, b_mat, nnz_num);
 }
 
-__global__ spgemmInnProMul() {
-  
+__global__ spgemmInnProMul()
+{
 }
