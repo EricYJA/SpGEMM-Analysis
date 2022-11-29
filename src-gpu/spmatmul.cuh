fcd1d4648ddd56a70350aa0371e5a540842f4fb7
Floruaaa666 <chenfei.yao.flora@gmail.com>
2022-11-28 22:30:40 -0800

    .

--- src-gpu/spmatmul.cuh
+++ src-gpu/spmatmul.cuh
@@ -6,6 +6,10 @@ __global__ spgemmRowWiseMul() {
   
 }
 
-__global__ spgemmInnProMul() {
-  
+__global__ void spgemmInnProMul(CSRMatDevice A, CSCMatDevice B) {
+    int csr_tid = threadIdx.x + blockDim.x * blockIdx.x;
+    int csc_tid = threadIdx.y + blockDim.y * blockIdx.y;
+
+    
+
 }
